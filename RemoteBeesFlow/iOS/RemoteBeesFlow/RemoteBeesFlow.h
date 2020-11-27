7c4613705ebf8c6475a785d3668f5b3c79b5c1cc
Worker Bee <worker@beehive.dev>
2020-11-27 09:48:01 -0600

    Add RemoteBeesFlow iOS project to workspace

---
+++ RemoteBeesFlow/iOS/RemoteBeesFlow/RemoteBeesFlow.h
@@ -0,0 +1,18 @@
+//
+//  RemoteBeesFlow.h
+//  RemoteBeesFlow
+//
+//  Created by Khuong on 11/27/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteBeesFlow.
+FOUNDATION_EXPORT double RemoteBeesFlowVersionNumber;
+
+//! Project version string for RemoteBeesFlow.
+FOUNDATION_EXPORT const unsigned char RemoteBeesFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteBeesFlow/PublicHeader.h>
+
+
