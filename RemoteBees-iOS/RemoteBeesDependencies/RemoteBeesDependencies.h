5678d010a3b5260a41afbb969b77dce80ba6e1a6
Worker Bee <worker@beehive.dev>
2020-11-27 09:57:29 -0600

    Setup RemoteBeesDependencies for sharing common dependencies.

---
+++ RemoteBees-iOS/RemoteBeesDependencies/RemoteBeesDependencies.h
@@ -0,0 +1,18 @@
+//
+//  RemoteBeesDependencies.h
+//  RemoteBeesDependencies
+//
+//  Created by Khuong on 11/27/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteBeesDependencies.
+FOUNDATION_EXPORT double RemoteBeesDependenciesVersionNumber;
+
+//! Project version string for RemoteBeesDependencies.
+FOUNDATION_EXPORT const unsigned char RemoteBeesDependenciesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteBeesDependencies/PublicHeader.h>
+
+
